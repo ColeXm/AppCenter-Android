dc505e4d0ec0eaea74a3025c0efc13f81f8f3d4e
REDMOND\colexia <colexia@microsoft.com>
2023-03-29 17:46:12 +0800

    1

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,11 +1,18 @@
-echo "cole-------------------------------------test"
+# echo "cole-------------------------------------test"
 
-echo "reinstall ruby"
-brew reinstall ruby@2.7
+# echo "reinstall ruby"
+# brew uninstall ruby@2.7
 
-echo "brew install ruby@2.7.4"
-brew install rbenv ruby-build
-rbenv init
-eval "$(rbenv init - zsh)"
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | zsh
-rbenv install 2.7.6
+# echo "brew install ruby@2.7.6"
+# brew install rbenv ruby-build
+# rbenv init
+# eval "$(rbenv init - zsh)"
+# curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | zsh
+# rbenv install 2.7.6
+
+# ruby -v
+
+# which -a ruby
+
+#To list all installed Ruby versions in rbenv:
+echo "Ruby Version: $(rbenv versions)"
